e11fe16f7c5498b163b3152bf30594a904a1d4c7
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-08-10 18:51:20 +0100

    Fix eslint error

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ const Layout = props => {
     <div className={`site-wrapper ${toggleNav ? `site-head-open` : ``}`}>
       <header className="site-head">
         <div className="site-head-container">
-          <a
+          <button
             className="nav-burger"
             href={`#`}
             onClick={() => setToggleNav(!toggleNav)}
@@ -23,7 +23,7 @@ const Layout = props => {
                 <div className="hamburger-inner" />
               </div>
             </div>
-          </a>
+          </button>
           <nav id="swup" className="site-head-left">
             <ul className="nav" role="menu">
               <li className="nav-home" role="menuitem">
