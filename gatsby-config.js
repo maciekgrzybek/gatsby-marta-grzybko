b48d98cb94993a5b30338c00bdddcf3d3536d043
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-08-17 23:02:55 +0100

    Remove old plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,12 +36,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: siteConfig.name,
