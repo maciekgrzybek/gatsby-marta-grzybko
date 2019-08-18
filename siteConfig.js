145eaa7ce7bef32a3e2710f9e76c27881b0c6851
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-08-18 20:39:00 +0100

    Update header

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Marta Grzybko',
   subtitle: 'Projktowane wnętrz',
   shortName: 'Marta Grzybko',
-  description: 'Przestrzeń wewnętrzna staje się rzeczywistością budynku.',
+  description: '"Przestrzeń wewnętrzna staje się rzeczywistością budynku."',
   url: 'https://martagrzybko.pl', // Domain of your site without prefix!
   prefix: '/',
   author: 'Maciek Grzybek',
