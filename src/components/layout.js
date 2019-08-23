20be5f68daab57ad661c91c19533a3f00d706c92
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-08-23 13:55:13 +0100

    Update

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import Header from './header';
 
 const Layout = ({ children }) => {
   const [navIsOpen, setToggleNav] = useState(false);
-
+  let classMain = navIsOpen ? `site-head-open site-wrapper ` : `site-wrapper`;
   const {site} = useStaticQuery(graphql`
     query LayoutQuery {
       site {
@@ -18,7 +18,7 @@ const Layout = ({ children }) => {
     }
   `);
   return (
-    <div className={`site-wrapper ${navIsOpen ? `site-head-open` : ``}`}>
+    <div className={classMain}>
       <Header siteMetadata={site.siteMetadata} toggleNav={setToggleNav} navIsOpen={navIsOpen}/>
       <main id="site-main" className="site-main">
         <div id="swup">
